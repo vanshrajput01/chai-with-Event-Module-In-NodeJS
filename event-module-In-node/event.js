e23d12b5dc30be76c467978eb4159d0c4e65e76e
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-18 16:23:01 +0530

    second commit

---
+++ event-module-In-node/event.js
@@ -0,0 +1,9 @@
+// Event Module in Node.js
+
+// Require event module
+
+const EventEmitter  = require("events");
+// Create object of EventEmitter class
+const event = new EventEmitter();
+
+console.log(event);
